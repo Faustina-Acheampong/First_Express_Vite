17d1057ba526b98a40d9524f6f1ddd2494780349
Faustina-Acheampong <157317178+Faustina-Acheampong@users.noreply.github.com>
2024-10-03 01:46:19 +0200

    Created aperson object.

--- server/server.js
+++ server/server.js
@@ -11,6 +11,19 @@ app.get("/api", (req, res) => {
     res.json({flowers: ["Rose", "Tulip", "Daisy", "Orchid", "Iris"] });
 });
 
+const person = {
+    name: "Edgar Johnson",
+    age: 30,
+    gender: "Male",
+    occupation: "Software Developer",
+    email: "edgar.johnson@gmail.com",
+    telephone: "123-456-7890"
+};
+
+app.get("/api/person", (req, res) => {
+    res.json(person);
+});
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
